fb7d30804ea6d04b17b4df07790bd4394c082424
Lbelus <lorris.belus@icloud.com>
2022-07-20 21:15:23 +0200

    first_run

---
+++ my_mastermind.c
@@ -0,0 +1,297 @@
+/*
+
+|                           ░░░░░░░░░░░░░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░                  ░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░    ▒▒░░░░  ░░▒▒▒▒    ░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░  ▒▒░░░░▒▒▒▒▒▒░░░░▒▒  ░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░  ░░▒▒░░░░░░░░░░░░░░▒▒░░  ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░  ▒▒░░▒▒░░▒▒▒▒▒▒░░░░░░▒▒  ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░  ▒▒░░▒▒▒▒░░▒▒▒▒▒▒▒▒░░▒▒  ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░                          ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░      ██████  ██████      ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░    ██████      ██████    ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░    ██████      ██████    ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░            ██            ░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░    ▒▒  ██  ██  ░░    ░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░          ░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░  ░░░░░░░░░░░░░░  ░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░  ░░  ░░  ░░  ░░  ░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░                      ░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░                  ░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░          ░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+|                           ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+|                           ▓▓▒▒▒▒░░▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░▓▓▓▓▓▓
+|    
+|    
+|    ███╗   ███╗██╗   ██╗    ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗██████╗ 
+|    ████╗ ████║╚██╗ ██╔╝    ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗
+|    ██╔████╔██║ ╚████╔╝     ██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║██║  ██║
+|    ██║╚██╔╝██║  ╚██╔╝      ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██║  ██║
+|    ██║ ╚═╝ ██║   ██║       ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██████╔╝
+|    ╚═╝     ╚═╝   ╚═╝       ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ 
+                                                                                                            
+source : https://textart.sh/topic/brain
+         https://patorjk.com/software/taag/#p=testall&f=Graffiti&t=My%20mastermind
+*/
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <stdbool.h>
+
+#define SIZE 4
+
+
+int randomizer(int *array){
+    for (int i = 0;i< SIZE; i++){
+        array[i] = rand()%12;
+        if(array[i] > 7){i--;}    
+    }
+    return *array;
+}
+
+void test_randomizer(int *param_1){
+    for(int i = 0;i < SIZE; i++){
+        printf("%i",param_1[i]);
+    }
+}
+
+void intro(){
+    char welcome_message[30] = "Will you find the secret code?";
+    write(1,welcome_message,30);
+    write(1,"\n",1);
+}
+
+int well_placed_test(char *param_1, int *param_2){
+    char well_placed_message[20] = "Well placed pieces: ";
+    int count = 0;
+    write(1,well_placed_message,20);
+    for(int i = 0; i < SIZE; i++){ 
+        if(param_1[i]-48 == param_2[i]){
+            //write(1,&param_1[i],1);
+            count++;
+        }
+    }
+    write(1,"\n",1);
+    return count;
+}
+
+void missplaced_test(char *input_string, int *secret_string, int n){
+    char old_input[1] = {0};
+    char missplaced_message[18] = "Misplaced pieces: ";
+    write(1,missplaced_message,18);
+    for(int x = 0; x < SIZE; x++){
+        for(int y = 0 + n; y < SIZE; y++){
+            if(input_string[x]-48 == secret_string[y] && input_string[x]-48 != secret_string[x] && old_input[0] != input_string[x] ){
+                //write(1,&input_string[x],1);
+                old_input[0] = input_string[x];
+            }
+        }
+    }
+    write(1,"\n",1);
+}
+
+
+int check_input(char *input_string, int *secret_string){
+    char old_input[1] = {0};
+    char missplaced_message[18] = "Misplaced pieces: ";
+    char well_placed_message[20] = "Well placed pieces: ";
+    char *buffer = 0;
+    int count = 0, n = 0;
+    bool bool_table[SIZE] = {false,false,false,false};
+
+    write(1,well_placed_message,20);
+    for(int i = 0; i < SIZE; i++){ 
+        if(input_string[i]-48 == secret_string[i]){
+            bool_table[i] = true;
+            count++;
+        }
+    }
+    
+    if(count > 0){
+        buffer = malloc(sizeof(int)*count);
+        for(int i = 0; i < count; i++){
+            if(input_string[i]-48 == secret_string[i]){
+                buffer[i] = input_string[i];
+            }
+        }
+    } 
+
+    n = count + 48; 
+    write(1,&n,1); write(1,"\n",1);
+
+//-------------------------------------------
+
+    n=0;
+    write(1,missplaced_message,18);
+    
+    for(int x = 0; x < SIZE; x++){
+        for(int y = 0; y < SIZE; y++){
+            if(bool_table[y] == false &&input_string[x]-48 == secret_string[y] && input_string[x]-48 != secret_string[x] && old_input[0] != input_string[x]){
+                old_input[0] = input_string[x];
+                n++;
+                bool_table[y] = true;
+            }
+        }
+    }
+
+    n = n + 48;
+    write(1,&n,1); 
+    write(1,"\n",1);
+    
+    return count;
+}
+
+int isnotint(char * param_1){
+    
+    for(int i = 0; i < SIZE; i++){
+        if ((param_1[i] < 48 || param_1[i] > 57)){
+            return 0;
+        } 
+    }
+    return 1;
+}
+
+int my_ctoi(char *param_1, size_t n){
+    int num = 0;
+    int mult =1;
+    n =(int)n < 0 ? -n : n; // check abs val
+    while (n--){
+        if((param_1[n] < '0' || param_1[n]> '9') && param_1[n] != '-'){
+            if(num){
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        if (param_1[n] == '-'){
+           if (num){
+                num = -num;
+                break;
+            }
+
+        } else {
+            num += (param_1[n]- '0')*mult;
+            mult *=10;
+
+        }
+    }
+    return num;
+}
+
+void victory(){
+    char victory_message[26] = "Congratz! you did it!";
+    write(1,victory_message,26);
+    write(1,"\n",1);
+}
+
+    // ref ITOA : https://www.strudel.org.uk/itoa/
+	char* their_itoa(int value, char* result, int base) {
+		// check that the base if valid
+		if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+		char* ptr = result, *ptr1 = result, tmp_char;
+		int tmp_value;
+
+		do {
+			tmp_value = value;
+			value /= base;
+			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+		} while ( value );
+
+		// Apply negative sign
+		if (tmp_value < 0) *ptr++ = '-';
+		*ptr-- = '\0';
+		while(ptr1 < ptr) {
+			tmp_char = *ptr;
+			*ptr--= *ptr1;
+			*ptr1++ = tmp_char;
+		}
+		return result;
+	}
+
+int my_strlen(char* param_1){
+        int i = 0;
+        while (param_1[i] != '\0'){
+            i++;
+        }
+        return(i);
+}
+
+
+int main(int argc, char** argv){
+    int nb_try = 0, i = 0, try = 10 ;
+    int problem[4] = {0};
+    srand(clock());
+    randomizer(problem);
+    intro();  
+
+    for(int y = 2; y < argc; y++){
+        i++;
+        if(argv[i][0] == '-'){
+            for(int x = 1; argv[i][x] != '\0'; x++ ){
+                char flag = argv[i][x];
+                switch(flag){
+                case 't':
+                    try = my_ctoi(argv[y],my_strlen(argv[y]));
+                break;
+                case 'c': 
+                    for (int z = 0;z< SIZE; z++){
+                        problem[z] = (int)argv[y][z]-48; 
+                        }
+                break;
+                }
+            }            
+        }
+    }
+    
+    while(nb_try < try){
+
+        write(1,"Round ",6);
+        
+        char buffer[5] = {0};
+        their_itoa(nb_try, buffer, 10);
+        int sizen = my_strlen(buffer);
+        write(1, buffer,sizen);
+        write(1,"\n",1);
+        int test = 0;
+        char user_input_buffer[SIZE] = {0};
+        char escape_catcher[10] = {0};
+        
+        while(test < 1){
+
+            read(0,user_input_buffer,4);
+            
+            if(isnotint(user_input_buffer) == 0){
+                write(1,"wrong input !\n",15);
+               
+            };
+            test = isnotint(user_input_buffer);
+            read(0,escape_catcher,10);
+            //write(1,"\n",1); 
+        }
+
+        int count_return = check_input(user_input_buffer, problem);
+        if(count_return == SIZE){ victory();return EXIT_SUCCESS;}
+        write(1,"---\n",4);
+        nb_try++;
+    }
+
+    return EXIT_FAILURE;
+}
